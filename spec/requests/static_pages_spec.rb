16bfcf3bf2992f4b5a39d76f4c0263293ae92cb5
Zhuohui Wang <woodpecker12@163.com>
2017-08-05 00:21:35 +0800

    add title to StatisPages and its test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,11 @@ describe "Static pages" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
+
+    it "should have title 'X Twitter | Home'" do
+      visit '/static_pages/home'
+      page.should have_title('X Twitter | Home')
+    end
   end
 
   describe "Help page" do
@@ -14,12 +19,22 @@ describe "Static pages" do
       visit '/static_pages/help'
       page.should have_content('Help')
     end
+
+    it "should have title 'X Twitter | Help'" do
+      visit '/static_pages/help'
+      page.should have_title('X Twitter | Help')
+    end
   end
 
   describe "About page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       page.should have_content('About Us')
+    end
+
+    it "should have title 'X Twitter | About'" do
+      visit '/static_pages/about'
+      page.should have_title('X Twitter | About')
     end
   end
 end
