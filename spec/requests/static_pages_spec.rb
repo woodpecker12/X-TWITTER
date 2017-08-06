a36978b43c8523649a5dd7e55278cf0f3dfc70d4
Zhuohui Wang <woodpecker12@163.com>
2017-08-06 18:40:41 +0800

    refactor StaticPages test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,54 +1,35 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  let(:base_title) { 'X Twitter |' }
+  subject { page }
 
   describe "Home page" do
-    it "should have the content 'Sample App'" do
-      visit root_path
-      page.should have_content('X Twitter')
-    end
+    before { visit root_path }
 
-    it "should have title 'X Twitter | Home'" do
-      visit root_path
-      page.should have_title("#{base_title} Home")
-    end
+    it { should have_content('X Twitter') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit help_path
-      page.should have_content('Help')
-    end
+    before { visit help_path }
 
-    it "should have title 'X Twitter | Help'" do
-      visit help_path
-      page.should have_title("#{base_title} Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
-    it "should have the content 'About Us'" do
-      visit about_path
-      page.should have_content('About Us')
-    end
+    before { visit about_path }
 
-    it "should have title 'X Twitter | About Us'" do
-      visit about_path
-      page.should have_title("#{base_title} About Us")
-    end
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
   end
 
   describe "Contact page" do
-    it "should have the content 'About Us'" do
-      visit contact_path
-      page.should have_content('Contact')
-    end
+    before { visit contact_path }
 
-    it "should have title 'X Twitter | Contact'" do
-      visit contact_path
-      page.should have_title("#{base_title} Contact")
-    end
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 
 end
