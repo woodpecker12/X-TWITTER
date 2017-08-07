0846fa7052fb348dc75958fbea3e231f4aed95c1
Zhuohui Wang <woodpecker12@163.com>
2017-08-07 23:38:12 +0800

    finish sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_secure_password
   before_save { self.email = email.downcase }
 
   validates :name, presence: true, length: { maximum: 50 }
@@ -8,6 +9,6 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
-  has_secure_password
-  
+
+
 end
